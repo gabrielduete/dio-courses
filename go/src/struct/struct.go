ed6be5117d9530cfce0e568adca6697daa04d5b1
gabrielduete <gabrielmonteiroduete@gmail.com>
2025-11-20 17:06:42 -0300

    feat: add struct

---
+++ go/src/struct/struct.go
@@ -0,0 +1,11 @@
+// type Person struct {
+//     name string
+//     lastname string
+//     age int8
+// }
+
+/*
+ person1 := Person{"name", "lastname", 24}
+ person2 := Person{name: "name", lastname: "lastname", age: 24}
+ person3 := Person{name: "name", age: 24}
+*/
