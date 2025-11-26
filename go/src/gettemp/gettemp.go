2c3afa9a459d8f0575c885c0f937ed7c28ba6c09
gabrielduete <gabrielmonteiroduete@gmail.com>
2025-11-25 23:15:08 -0300

    refactor: simplify variable declarations in main function

--- go/src/gettemp/gettemp.go
+++ go/src/gettemp/gettemp.go
@@ -5,8 +5,11 @@ import "fmt"
 const ebullitionF float64 = 212.0
 
 func main() {
-	var tempF float64 = ebullitionF
-	var tempC float64 = (tempF - 32) * 5 / 9
+	// var tempF float64 = ebullitionF
+	tempF := ebullitionF
+
+	// var tempC float64 = (tempF - 32) * 5 / 9
+	tempC := (tempF - 32) * 5 / 9
 
 	fmt.Println("Temp ebullition water in Fahrenheit", tempF)
 	fmt.Println("Temp ebullition water in Celsius", tempC)
