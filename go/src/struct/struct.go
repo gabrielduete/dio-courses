885ceefe6cf31574ef4521cc4a62e68e845504ee
gabrielduete <gabrielmonteiroduete@gmail.com>
2026-02-04 01:39:55 -0300

    feat: add method

--- go/src/struct/struct.go
+++ go/src/struct/struct.go
@@ -8,4 +8,5 @@
  person1 := Person{"name", "lastname", 24}
  person2 := Person{name: "name", lastname: "lastname", age: 24}
  person3 := Person{name: "name", age: 24}
+ fmt.Println(Person{"Alice", 30}) // Output: {Alice 30}
 */
